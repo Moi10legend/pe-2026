2671f2b7c6bff2b7e7bae819d703a7265be22411
Moisés Freire <125685929+Moi10legend@users.noreply.github.com>
2026-03-02 11:51:54 +0000

    atualização media idades

--- mediaidades.c
+++ mediaidades.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
+#define MAX_QTD 5
 
 int main() {
-    int idade, qtdPessoas = 0, soma = 0, idades[1000] = {0}, maxQtd = 5;
+    int idade, qtdPessoas = 0, soma = 0, idades[1000] = {0};
     double media;
     printf("Digite sua idade: ");
     scanf("%d", &idade);
-    for(int i = 1; i < maxQtd; i += 1) {
-        qtdPessoas += 1;
+    for(int i = 0; i < MAX_QTD; i += 1) {
         soma += idade;
         scanf("%d", &idade);
         idades[i] = idade;
     }
     
-    media = (double) soma / (double) qtdPessoas;
+    media = (double) soma / (double) MAX_QTD;
     printf("A média de idade dos participantes é de %.2lf anos\n", media);
-    for(int i = 1; i < maxQtd; i += 1){
+    for(int i = 0; i < maxQtd; i += 1){
     	if(idades[i] > media){
     		printf("%d \n", idades[i]);   
 		}
